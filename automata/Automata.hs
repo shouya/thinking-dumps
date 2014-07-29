5d2cb8e4853628ff8d97ac0ce03477e4275c82b3
Shou Ya <shouyatf@gmail.com>
2014-07-30 06:23:49 +0800

    new: automata

---
+++ automata/Automata.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE GADTs #-}
+
+import qualified Data.Set as S
+
+class State a where
+  startState :: a
+  acceptStates :: S.Set a
+  failState :: a
+
+class Input i where
+  epsilon :: i
+
+
+data DFA s i where
+  DFA :: (State s, Input i) => (s -> i -> s) -> DFA s i
+
+
+
+evalDFA :: DFA s i -> [i] -> s
+evalDFA (DFA d) xs = foldl d startState xs
+
+
+-- Example Input
+data MyInput = Zero | One | Epsilon deriving Show
+instance Input MyInput where
+  epsilon = Epsilon
+
+-- Example State
+data MyState = A | B | C | D | E | F deriving Show
+instance State MyState where
+  startState = A
+  acceptStates = S.singleton F
+  failState = F
+
+
+myDFA :: DFA MyState MyInput
+myDFA = DFA myDelta where
+  myDelta A One  = B
+  myDelta A Zero = C
+  myDelta B One  = D
+  myDelta B Zero = C
+  myDelta C One  = D
+  myDelta _ _    = F
---
+++ automata/README.md
@@ -0,0 +1,12 @@
+# Automata
+
+## Resoucres
+* [Coursera: Automata](https://class.coursera.org/automata-002/)
+
+## Files
+
+* `Automata.hs`: A library for State, Input, DFA, and NFA classes/data
+types.
+* `DFA.hs`: A series of functions for DFA execution/test.
+* `NFA.hs`: A series of functions about NFA execusion and NFA to DFA
+conversion.
