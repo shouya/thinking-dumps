6b84103a7f6d558091adaecc2ba5e779a7aea6df
Shou Ya <shouyatf@gmail.com>
2014-08-21 19:05:30 +0800

    added theorems about {Proper,} R-{ancestor,posterity}

--- imp/Chap3.v
+++ imp/Chap3.v
@@ -57,3 +57,49 @@ Proof.
 Qed.
 (* Prooving inductivity with inductive assumption is actually meaningless.
    So they're just demonstrations *)
+
+
+(* page 25:
+A property is called “R-hereditary” when, if it belongs to a term x, and x
+has the relation R to y, then it belongs to y.
+*)
+Definition r_hereditary {X} (R : relation X) (p : X -> Prop) : Prop :=
+  forall x y, p x -> R x y -> p y.
+
+(*
+A term x is said to be an “R-ancestor” of the term y if y has every
+R-hereditary property that x has, provided x is a term which has the
+relation R to something or to which something has the relation R.
+(This is only to exclude trivial cases.)
+*)
+
+Definition r_ancestor {X} (R : relation X) (x : X) (y : X) :=
+  forall (p : X -> Prop), r_hereditary R p -> p x -> p y.
+
+(* My own understandings *)
+Example __ex1 : r_hereditary lt (fun x => x > 0).
+Proof.
+  unfold r_hereditary.
+  unfold lt.
+  intros.
+  induction H0.
+  induction H. constructor. constructor.
+  constructor. apply IHle.
+  constructor. apply IHle.
+Qed.
+
+Example __ex2 : r_ancestor lt 3 5.
+Proof.
+  unfold r_ancestor.
+  unfold r_hereditary.
+  intros.
+  apply H with 3.
+  assumption. auto.
+Qed.
+
+
+(* page 26:
+The “R-posterity” of x is all the terms of which x is an R-ancestor.
+*)
+
+Definition r_posterity {X} (R : relation X) (x : X) (y : X) := r_ancestor R y x.
--- imp/Chap4.v
+++ imp/Chap4.v
@@ -5,6 +5,8 @@ Require Import Relations.
 
 (* Ordered Series *)
 
+Load "Chap3.v".
+
 
 Definition asymmetrical {X : Type} (R : relation X) : Prop :=
   forall x y, R x y -> ~(R y x).
@@ -174,4 +176,89 @@ Proof.
   intros. intro.
   apply H0 with x.
   apply H with y; assumption.
+Qed.
+
+(* A relation is connected when, given any two different terms of its field,
+the relation holds between the first and the second or between the second and
+the first (not excluding the possibility that both may happen, though both
+cannot happen if the relation is asymmetrical).
+*)
+
+(* Defined in the beginning as definition of 'connected' *)
+
+
+(* A relation is serial when it is an aliorelative, transitive,
+and connected; or, what is equivalent, when it is asymmetrical,
+transitive, and connected.
+*)
+
+Definition serial {X} (R : relation X) : Prop :=
+  aliorelative R /\ transitive R /\ connected R.
+Definition serial' {X} (R : relation X) : Prop :=
+  asymmetrical R /\ transitive R /\ connected R.
+
+
+(*Definition proper_posterity {X} (R : relation X) *)
+
+(* page 36:
+The “proper posterity” of x with respect to R consists of
+all terms that possess every R-hereditary property possessed by
+every term to which x has the relation R.
+*)
+
+(* method 1: defining Type
+Definition proper_posterity {X} (R : relation X) (x : X) :=
+  { y | forall (p : X -> Prop), r_hereditary R p -> R y x }.
+*)
+(* method 2: defining Prop *)
+
+Definition proper_posterity {X} (R : relation X) (x : X) : X -> Prop :=
+  fun y => forall p, r_hereditary R p -> (forall t, R x t -> p t) -> p y.
+
+(*
+Definition proper_posterity {X} (R : relation X) (x : X) (y : X) : Prop :=
+  r_posterity R y x /\ R y x.
+*)
+
+(* A term x is a “proper ancestor” of y with respect to R if y
+belongs to the proper posterity of x with respect to R.
+*)
+
+Definition proper_ancestor {X} (R : relation X) (x : X) (y : X) : Prop :=
+  proper_posterity R y x.
+
+
+(* It will always be transitive: no matter what sort of relation
+   R may be, “R-ancestor” and “proper R-ancestor” are always both transitive.
+*)
+
+Theorem r_anc_trans {X} :
+  forall R : relation X, transitive (r_ancestor R).
+Proof.
+  intros.
+  unfold transitive. unfold r_ancestor.
+  intros.
+  apply H0. exact H1.
+  apply H. exact H1.
+  assumption.
+Qed.
+
+Theorem prop_anc_trans {X} :
+  forall R : relation X, transitive (proper_ancestor R).
+Proof.
+  intros.
+  unfold transitive. unfold proper_ancestor. unfold proper_posterity.
+  intros.
+
+  apply H.
+  exact H1.
+  intros.
+  unfold r_hereditary in H1.
+  apply H1 with y.
+
+  apply H0.
+  exact H1.
+  intros. apply H2. assumption.
+
+  assumption.
 Qed.
