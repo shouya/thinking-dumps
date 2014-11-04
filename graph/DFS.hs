90111b39b92020848d44b705462d706aeec80aee
Shou Ya <shouyatf@gmail.com>
2014-11-04 22:26:09 +0800

    dfs search

--- graph/DFS.hs
+++ graph/DFS.hs
@@ -2,14 +2,25 @@ module DFS where
 
 import Data.List ((\\), sort)
 import Data.Array ((!))
-import Data.Maybe (listToMaybe)
 import Data.Graph
 
 dfsTraverse :: Graph -> Vertex -> [Vertex]
-dfsTraverse graph start = dfs' [start] [start]
+dfsTraverse graph begin = reverse $ dfs' [begin] [begin]
   where dfs' (x:xs) visited =
           case pickUnvisited x visited of
-            Just v  -> dfs' (v:x:xs) (v:visited)
-            Nothing -> dfs' xs visited
-        dfs' [] visited    = reverse visited
-        pickUnvisited x visited = listToMaybe $ sort (graph ! x \\ visited)
+            (v:_) -> dfs' (v:x:xs) (v:visited)
+            []    -> dfs' xs visited
+        dfs' [] visited    = visited
+        pickUnvisited x visited = sort (graph ! x \\ visited)
+
+
+dfsSearch :: Graph -> Vertex -> Vertex -> [Vertex]
+dfsSearch graph begin end = reverse $ dfs' [begin] [begin]
+  where dfs' (x:xs) visited
+          | x == end  = visited
+          | otherwise =
+            case pickUnvisited x visited of
+              (v:_) -> dfs' (v:x:xs) (v:visited)
+              []    -> dfs' xs       visited
+        dfs' [] visited = visited
+        pickUnvisited x visited = sort (graph ! x \\ visited)
--- graph/DFSTest.hs
+++ graph/DFSTest.hs
@@ -3,25 +3,43 @@ module DFSTest where
 import DFS
 import Data.Graph hiding (edges)
 import Control.Monad (liftM2)
-
 import Test.QuickCheck
 
 
-exampleGraph :: Graph
-exampleGraph = buildG bounds (edges ++ rev edges)
-  where edges    = [(1,2), (2,3), (2,5), (3,4), (4,5)]
-        rev      = map (\(a,b) -> (b,a))
-        bounds = liftM2 (,) (foldl1 min) (foldl1 max) $
-                 (map fst edges) ++ (map snd edges)
 
 {-
 exampleGraph:
 
-1--2--3--4
-    \    |
-     +---5
+  1--2--3--4
+      \    |
+       ----5
+
 -}
+
+edges :: [Edge]
+edges = [(1,2), (2,3), (2,5), (3,4), (4,5)]
+
+bounds :: Bounds
+bounds = liftM2 (,) (foldl1 min) (foldl1 max) $
+         (map fst edges) ++ (map snd edges)
+
+exampleGraph :: Graph
+exampleGraph = buildG bounds (edges ++ rev edges)
+  where rev      = map (\(a,b) -> (b,a))
+
+
+prop_SearchEqvTraverse :: Property
+prop_SearchEqvTraverse =
+  forAll inBounds $ \beg ->
+  forAll inBounds $ \end ->
+  let sResult = dfsSearch exampleGraph beg end
+      tResult = dfsTraverse exampleGraph beg
+  in sResult == (takeWhile (/=end) tResult) ++ [end]
+  where inBounds = choose bounds
+
+
 main :: IO ()
 main = do quickCheck (dfsTraverse exampleGraph 1 == [1..5])
           quickCheck (dfsTraverse exampleGraph 2 == [2,1,3,4,5])
           quickCheck (dfsTraverse exampleGraph 3 == [3,2,1,5,4])
+          quickCheck prop_SearchEqvTraverse
