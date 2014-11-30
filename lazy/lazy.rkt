7bf5b5cb3ce4d984ee8ddef753c3ffba783dce1b
Shou Ya <shouyatf@gmail.com>
2014-11-30 19:34:00 +0800

    initialized lazy evaluation implementation

--- lazy/lazy.rkt
+++ lazy/lazy.rkt
@@ -1,9 +1,34 @@
 #lang racket
 
+(define (eval! expr env)
+  (undefined))
+
 (define eval eval!)
 
-(define (eval! expr env)
-  (case (car expr)
-    ['+ (eval (+ (cadr expr) (caddr expr)))]
-    ['except (error "aha! mia G point got touched!")]
-    ))
+(define (built-in-op? sym)
+  (member sym (map car built-in-symbol-map)))
+
+
+(define (normal-form? expr)
+  (case (type-of expr)
+    ['num  #t]
+    ['λ    #t]
+    ['appl #f]))
+
+(define (getval expr)
+  (if (normal-form? expr)
+      (cdr expr)
+      (error "not value in normal form")))
+
+
+(define type-of car)
+
+(define my-prog
+  '[appl (λ '+) (num 1) (num 2)])
+
+(define built-in-symbol-map
+  '([+      (λ (rest) (+ (getval (eval (cadr rest)))
+                         (getval (eval (cadr rest)))))]
+    [except (λ (rest) (error "aha! mia G point got touched!"))]))
+  
+(define (undefined) (error "meow"))
