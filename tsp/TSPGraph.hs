89fb0d2fbf65c7a9c75a3dc07b8cacc462dcbb0a
Shou Ya <shouyatf@gmail.com>
2014-12-21 01:35:24 +0800

    finished visualize lib

--- tsp/TSPGraph.hs
+++ tsp/TSPGraph.hs
@@ -1,5 +1,9 @@
-
 import Graphics.Gloss
+import TSPLib
+import Data.Monoid
+import Data.Functor
+import Control.Arrow ((***))
+
 
 imgWidth, imgHeight :: Int
 imgWidth  = 600
@@ -7,5 +11,59 @@ imgHeight = 600
 
 
 main :: IO ()
-main = display window white (Circle 80)
-  where window = InWindow "TSP Visualize" (imgWidth, imgHeight) (10, 10)
+main = do
+  (nodes, edges) <- parseInput
+  let window = InWindow "TSP Visualize" (imgWidth, imgHeight) (10, 10)
+      (halfW, halfH) = (fromIntegral (-imgWidth)/2, fromIntegral (-imgHeight)/2)
+      translatedPic  = scale 0.9 (-0.9) $ translate halfW halfH pic
+      (xrng, yrng)   = (xRange nodes, yRange nodes)
+      pic = paintNodes nodes xrng yrng <>
+            paintEdges edges xrng yrng
+    in display window white translatedPic
+
+
+scaleVal :: (Fractional a) => (a, a) -> a -> a -> a
+scaleVal (xmin, xmax) scl x = scl * (x - xmin) / (xmax - xmin)
+
+tupleFromIntegral :: (Num c, Num d, Integral a, Integral b) =>
+                     (a, b) -> (c, d)
+tupleFromIntegral = fromIntegral *** fromIntegral
+
+scaleVal' :: (Int, Int) -> Int -> Int -> Float
+scaleVal' rng scl = scaleVal fltrng fltscl . fromIntegral
+  where fltrng = tupleFromIntegral rng
+        fltscl = fromIntegral scl
+
+scaleTuple :: (Int, Int) -> (Int, Int) -> Int -> Int ->
+              (Int, Int) -> (Float, Float)
+scaleTuple xrng yrng xmax ymax = scaleX *** scaleY
+  where scaleX = scaleVal' xrng xmax
+        scaleY = scaleVal' yrng ymax
+
+paintNodes :: [Node] -> (Int,Int) -> (Int,Int) -> Picture
+paintNodes []  _ _  = blank
+paintNodes [_] _ _  = blank
+paintNodes ns xr yr = pictures $ map paintNode ns
+  where scalePoint = scaleTuple xr yr imgWidth imgHeight
+        paintNode node = uncurry translate (scalePoint node) $ circle 5
+
+
+
+paintEdges :: [Edge] -> (Int,Int) -> (Int,Int) -> Picture
+paintEdges []  _ _  = blank
+paintEdges [_] _ _  = blank
+paintEdges ns xr yr = pictures $ map paintEdge ns
+  where scalePoint = scaleTuple xr yr imgWidth imgHeight
+        paintEdge (p1, p2) = line $ [scalePoint p1, scalePoint p2]
+
+
+
+parseInput :: IO ([Node], [Edge])
+parseInput = do
+  (cfg:raw) <- lines <$> getContents
+  let [nn, _] = map read $ words cfg
+      rest = map (map read . words) raw
+      (ns, es) = splitAt nn rest
+      nodes = map (\[a,b] -> (a,b)) ns
+      edges = map (\[a,b,c,d] -> ((a,b), (c,d))) es
+    in return (nodes, edges)
--- tsp/TSPLib.hs
+++ tsp/TSPLib.hs
@@ -1,9 +1,20 @@
-module TSPLib where
+module TSPLib (
+  Node,
+  Edge,
+  distance,
+  xRange,
+  yRange,
+  parseString,
+  parseStdin,
+  parseFile,
+  outputForGraph
+  ) where
 
 import Data.Functor
 import Data.List
 import Control.Monad
-import Control.Arrow ((>>>))
+import Control.Applicative ((<*>), (<$>))
+import Control.Arrow ((>>>), (&&&))
 
 
 -- node
@@ -13,6 +24,10 @@ type Edge = (Node, Node)
 distance :: Node -> Node -> Double
 distance (x1,y1) (x2,y2) = sqrt (fromIntegral $ (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
 
+xRange  :: [Node] -> (Int, Int)
+xRange = map fst >>> (foldl1' min &&& foldl1' max)
+yRange :: [Node] -> (Int, Int)
+yRange = map fst >>> (foldl1' min &&& foldl1' max)
 
 
 parseString :: String -> [Node]
@@ -22,7 +37,7 @@ parseString =
   filter (not . null) >>>
   map words >>>
   map (map read) >>>
-  filter (\x -> length x /= 2) >>>
+  filter (\x -> length x == 2) >>>
   map (\[a,b] -> (a,b))
 
 
@@ -31,3 +46,16 @@ parseStdin = parseString <$> getContents
 
 parseFile :: FilePath -> IO [Node]
 parseFile path = parseString <$> readFile path
+
+
+outputForGraph :: [Node] -> [Edge] -> IO ()
+outputForGraph ns es = do
+  putStrLn $ show (length ns) ++ " " ++ show (length es)
+  mapM_ (putStrLn . showNode) ns
+  mapM_ (putStrLn . showEdge) es
+  where showNode (a,b) = show a ++ " " ++ show b
+        showEdge ((a,b),(c,d)) = show a ++ " " ++ show b ++ " " ++
+                                 show c ++ " " ++ show d
+
+main :: IO ()
+main = parseStdin >>= flip outputForGraph []
