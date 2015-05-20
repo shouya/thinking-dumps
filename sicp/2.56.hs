1cc2d4d1299a28fd9f9579e4087b5efbf1b627e1
Shou Ya <shouyatf@gmail.com>
2015-05-20 05:29:31 -0400

    simplify expression

--- sicp/2.56.hs
+++ sicp/2.56.hs
@@ -1,16 +1,15 @@
-
-
 type Var = String
 
 data Expr = Sum Expr Expr
           | Prod Expr Expr
           | I Integer
           | V Var
+          deriving Eq
 
 instance Show Expr where
   show (I i)
-    | i < 0 = "(" ++ show i ++ ")"
-    | _     = show i
+    | i < 0     = "(" ++ show i ++ ")"
+    | otherwise = show i
   show (V x) = x
   show (Prod s@(Sum _ _) t@(Sum _ _)) = "(" ++ show s ++ ")*" ++
                                         "(" ++ show t ++ ")"
@@ -21,9 +20,36 @@ instance Show Expr where
 
 
 derivative :: Expr -> Var -> Expr
-derivative (Sum  u v) x = Sum (derivative u x) (derivative v x)
-derivative (Prod u v) x = Sum udv vdu
-  where udv = Prod u $ derivative v x
-        vdu = Prod v $ derivative u x
-derivative (I _) x = 0
-derivative (V x') x = if x == x' then 1 else 0 -- partial derivative
+derivative (Sum  u v) x = mkS (derivative u x) (derivative v x)
+derivative (Prod u v) x = mkS udv vdu
+  where udv = mkP u $ derivative v x
+        vdu = mkP v $ derivative u x
+derivative (I _) _ = I 0
+derivative (V x') x = I $ if x == x' then 1 else 0 -- partial derivative
+
+
+mkS :: Expr -> Expr -> Expr
+mkS (I 0) a = a
+mkS a (I 0) = a
+mkS (I a) (I b) = I (a + b)
+mkS a b
+  | a == b    = mkP (I 2) a
+  | otherwise = Sum a b
+
+
+mkP :: Expr -> Expr -> Expr
+mkP (I 1) a = a
+mkP a (I 1) = a
+mkP (I 0) _ = I 0
+mkP _ (I 0) = I 0
+mkP (I a) (I b) = I (a*b)
+mkP a b = Prod a b
+
+
+
+
+
+main :: IO ()
+main = do
+  let expr = Prod (V "x") (V "x")
+    in print (derivative expr "x")
