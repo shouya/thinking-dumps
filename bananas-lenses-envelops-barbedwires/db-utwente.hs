26fca0f55bf5019f4947ad79d15ad4cfeee87077
Shou Ya <shouyatf@gmail.com>
2015-05-26 07:22:50 -0400

    paramorphism

--- bananas-lenses-envelops-barbedwires/db-utwente.hs
+++ bananas-lenses-envelops-barbedwires/db-utwente.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances #-}
 -- This is a note playing with the concepts to deepen my understanding
 -- while reading the paper: Functional Programming with Bananas,
 -- Lenses, Evelopes and Barbed Wire by Erik Meijer, et al.
@@ -52,3 +53,47 @@ fac'hylo :: Int -> Int
 fac'hylo = hylo 1 (*) g p
   where g c  = (c, c - 1) -- (c, c')
         p c' = c' == 0
+
+data Fix f = In (f (Fix f))
+data List a l = Nil | Cons a l
+type ListF a  = Fix (List a)
+
+instance Functor (List a) where
+  fmap f Nil        = Nil
+  fmap f (Cons a l) = Cons a (f l)
+
+hsToLst :: [a] -> ListF a
+hsToLst []     = In Nil
+hsToLst (x:xs) = In (Cons x (hsToLst xs))
+
+lstToHs :: ListF a -> [a]
+lstToHs (In Nil) = []
+lstToHs (In (Cons x xs)) = x : lstToHs xs
+
+data Number n = Zero | Succ n
+type NumberF = Fix Number
+
+instance Functor Number where
+  fmap f Zero     = Zero
+  fmap f (Succ n) = Succ (f n)
+
+hsToInt :: Int -> NumberF
+hsToInt 0 = In Zero
+hsToInt n = In (Succ (hsToInt $ n - 1))
+
+intToHs :: NumberF -> Int
+intToHs (In Zero) = 0
+intToHs (In (Succ n)) = succ (intToHs n)
+
+para :: b -> (a -> [a] -> b -> b) -> ([a] -> b)
+para b f []     = b
+para b f (a:as) = f a as (para b f as)
+
+paraGen :: (Functor f) => (f (Fix f, b) -> b) -> (Fix f -> b)
+paraGen f (In as) = f (fmap keepCopy as)
+  where keepCopy a = (a, paraGen f a)
+
+-- Error: Insufficient IQ
+-- fac'para :: NumberF -> Int
+-- fac'para = paraGen f 1
+--   where f (n, m) = _
