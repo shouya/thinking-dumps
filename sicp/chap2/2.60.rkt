4151fb611a0da15df467314883189e4b138a66f9
Shou Ya <shouyatf@gmail.com>
2015-07-28 17:56:16 -0400

    2.6

---
+++ sicp/chap2/2.60.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond [(null? set) #f]
+        [(equal? x (car set)) #t]
+        [else (element-of-set? x (cdr set))]))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+
+(define (intersection-set set1 set2)
+  (cond [(or (null? set1) (null? set2)) '()]
+        [(element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2))]
+        [else (intersection-set (cdr set1) set2)]))
+
+;; eff dep on `append`, O(N)
+(define (union-set set1 set2) (append set1 set2))
