78bff999991ccbd3dbed401430f006cfc08685e5
Shou Ya <shouyatf@gmail.com>
2015-08-02 06:39:47 -0400

    2.77 & 2.78

---
+++ sicp/chap2/2.77.rkt
@@ -0,0 +1,8 @@
+;; Of course this works. before adding these bindings it's unable to
+;; access these functions via the generic interface. So what we do is
+;; just register these bindings.
+;;
+;;   (magnitude z)
+;;   (magnitude-rectangle z)
+;;   (sqrt (+ (* a a) (* b b)))
+;;
---
+++ sicp/chap2/2.78.rkt
@@ -0,0 +1,12 @@
+
+(define (attach-tag tag value)
+  (if (eq? tag 'number)
+      value
+      (cons tag value)))
+
+(define (type-tag datum)
+  (if (number? datum) 'number
+      (car datum)))
+(define (contents datum)
+  (if (number? datum) datum
+      (cdr datum)))
