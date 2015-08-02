c5392566659de2ddf04946f83e3caf6c43a971ca
Shou Ya <shouyatf@gmail.com>
2015-08-02 09:06:47 -0400

    2.80

---
+++ sicp/chap2/2.80.rkt
@@ -0,0 +1,25 @@
+
+;; scheme number package
+
+(define (=zero?-scheme-number x)
+  (eqv? x 0))
+
+(put '=zero? '(scheme-number) =zero?-scheme-number)
+
+
+;; rational number package
+
+(define (=zero?-rational-number x)
+  (match x
+    [(list num den) (eqv? num 0)]))
+
+(put '=zero? '(rational) =zero?-rational-number)
+
+
+;; complex number package
+
+(define (=zero?-complex-number x)
+  (let ([a (real-part x)] [b (imag-part x)])
+    (and (eqv? a 0) (eqv? b 0))))
+
+(put '=zero? '(complex) =zero?-complex-number)
