04fa04cef4651d9a3dfcf84ae6badf1615f805b1
Shou Ya <shouyatf@gmail.com>
2015-08-04 17:59:23 -0400

    2.89 revised

--- sicp/chap2/2.89.rkt
+++ sicp/chap2/2.89.rkt
@@ -19,3 +19,29 @@
 ;; for which we only record the coefficients and
 ;; we have order(p) = length(p) - 1
 ;;
+
+;; here's the impl
+
+(define (make-poly variable term-list)
+  (cons variable term-list))
+(define (variable p) (car p))
+(define (term-list p) (cdr p))
+;; continued on next page
+
+(define (adjoin-term term term-list)
+  (if (=zero? (order term))
+      (cons (coeff term) term-list)
+      (cons (car term-list)
+            (adjoin-term (make-term (- (order term) 1)
+                                    (coeff term))
+                         (cdr term-list)))))
+
+(define (the-empty-termlist) '())
+(define (first-term term-list)
+  (make-term (- (length term-list) 1)
+             (car term-list)))
+(define (rest-terms term-list) (cdr term-list))
+(define (empty-termlist? term-list) (null? term-list))
+(define (make-term order coeff) (cons order coeff))
+(define (order term) (car term))
+(define (coeff term) (cdr term))
---
+++ sicp/chap2/2.90.rkt
@@ -0,0 +1,16 @@
+(require "alg.rkt")
+
+(define (div-terms L1 L2)
+  (if (empty-termlist? L1)
+      (list (the-empty-termlist) (the-empty-termlist))
+      (let ((t1 (first-term L1))
+            (t2 (first-term L2)))
+        (if (> (order t2) (order t1))
+            (list (the-empty-termlist) L1)
+            (let ((new-c (div (coeff t1) (coeff t2)))
+                  (new-o (- (order t1) (order t2))))
+              (let ((rest-of-result
+                     <compute rest of result recursively>
+                     ))
+                <form complete result>
+                ))))))
