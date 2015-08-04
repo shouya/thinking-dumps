5016562a7dba7c3df9b67b0f8e27bea9051d31ec
Shou Ya <shouyatf@gmail.com>
2015-08-04 15:07:11 -0400

    2.86

---
+++ sicp/chap2/2.86.rkt
@@ -0,0 +1,14 @@
+;;
+;; we can just set complex as a structure with
+;; two numeral components. this is the same as,
+;; for example, (define (rect len wid) ...), for
+;; which the length and width could either be
+;; integers, rationals, or reals.
+;;
+
+
+;;
+;; we just let `sine` and `cosine` function to accept
+;; a number and try to convert them into a real number
+;; and then do calculation.
+;;
