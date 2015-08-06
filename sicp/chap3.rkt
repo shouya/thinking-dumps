b8767a78f4ef6c8c5c54163a6abe88864d1bcdfd
Shou Ya <shouyatf@gmail.com>
2015-08-06 17:40:20 -0400

    3.5

--- sicp/chap3.rkt
+++ sicp/chap3.rkt
@@ -59,3 +59,26 @@
           (if (> mistake-count 7)
               (error "☎︎110") ; or call-the-cops
               (error "wrong password!"))))))
+
+
+;; ex 3.5 monte carlo integration
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (* (random) range))))
+
+(define (estimate-integral p x1 x2 y1 y2 trials)
+  (define (new-p)
+    (let ([x (random-in-range x1 x2)]
+          [y (random-in-range y1 y2)])
+      (p x y)))
+  (monte-carlo trials new-p))
