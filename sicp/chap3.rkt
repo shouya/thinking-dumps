06c82a55067b72b3fd76c742f7cb872b3ec7707d
Shou Ya <shouyatf@gmail.com>
2015-08-13 18:12:12 -0400

    wire functions

--- sicp/chap3.rkt
+++ sicp/chap3.rkt
@@ -655,12 +655,29 @@ becomes:
 ;; recurse, which will still call the original function back.
 ;; however, there will be no efficiency improvement.
 
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
 (define (inverter in out)
   (define (invert-input)
-    (let ((new-val (logical-not (get-signal input))))
+    (let ((new-val (logical-not (get-signal in))))
       (after-delay inverter-delay
-                   (lambda () (set-signal! output new-val)))))
-  (add-action! input invert-input)
+                   (lambda () (set-signal! out new-val)))))
+  (add-action! in invert-input)
   'ok)
 
 (define (logical-not s)
@@ -673,7 +690,7 @@ becomes:
     (let ([new-val
            (logical-and (get-signal a1) (get-signal a2))])
       (after-delay and-gate-delay
-                   (lambda () (set-signal! output new-val)))))
+                   (lambda () (set-signal! out new-val)))))
   (add-action! a1 and-action-procedure)
   (add-action! a2 and-action-procedure)
   'ok)
@@ -684,7 +701,7 @@ becomes:
     [(1 0) 0]
     [(0 0) 0]
     [(0 1) 0]
-    [(_ _) (error "invalid signal" s)]))
+    [(_ _) (error "invalid signal" a b)]))
 
 
 ;; ex 3.28 define or-gate
@@ -693,7 +710,7 @@ becomes:
     (let ([new-val
            (logical-or (get-signal a1) (get-signal a2))])
       (after-delay or-gate-delay
-                   (lambda () (set-signal! output new-val)))))
+                   (lambda () (set-signal! out new-val)))))
   (add-action! a1 and-action-procedure)
   (add-action! a2 and-action-procedure)
   'ok)
@@ -704,7 +721,7 @@ becomes:
     [(1 0) 1]
     [(0 0) 0]
     [(0 1) 1]
-    [(_ _) (error "invalid signal" s)]))
+    [(_ _) (error "invalid signal" a b)]))
 
 
 ;; ex 3.29 define or-gate using and-gate and inverter
@@ -729,3 +746,106 @@ becomes:
         (full-adder a b prev-c-out s c-out)
         (ripple-carry-adder ass bss c-in sss prev-c-out)))
   'ok)
+
+;;; connect-wire isn't from the book
+(define (connect-wire a b)
+  (add-action! a (λ () (set-signal! b (get-signal a))))
+  (add-action! b (λ () (set-signal! a (get-signal b))))
+  'ok)
+
+
+;; time delayed:
+;;
+;; ha = o + 2a + i
+;; fa = 2ha + o = 3o + 4a + 2i
+;; ripple = n*fa = 3n*o + 4n*a + 2n*i
+;;
+
+
+;; wire function
+
+(define (make-wire)
+  (let ([signal-value 0]
+        [action-procedures '()])
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+
+;; agenda
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+;; simple simulation
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display "  New-value = ")
+                 (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define (probe-example-1)
+  (define input-1 (make-wire))
+  (define input-2 (make-wire))
+  (define sum (make-wire))
+  (define carry (make-wire))
+  (probe 'sum sum)
+  (probe 'carry carry))
+
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
