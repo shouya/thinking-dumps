bcd70e48f2887429a0e711be288c4f5be13eeae2
Shou Ya <shouyatf@gmail.com>
2015-08-12 20:23:14 -0400

    3.28

--- sicp/chap3.rkt
+++ sicp/chap3.rkt
@@ -654,3 +654,55 @@ becomes:
 ;; (memoize fib) will work because in `fib` it calls `fib` to
 ;; recurse, which will still call the original function back.
 ;; however, there will be no efficiency improvement.
+
+(define (inverter in out)
+  (define (invert-input)
+    (let ((new-val (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda () (set-signal! output new-val)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond [(= s 0) 1]
+        [(= s 1) 0]
+        [else (error "invalid signal" s)]))
+
+(define (and-gate a1 a2 out)
+  (define (and-action-procedure)
+    (let ([new-val
+           (logical-and (get-signal a1) (get-signal a2))])
+      (after-delay and-gate-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and a b)
+  (match* (a b)
+    [(1 1) 1]
+    [(1 0) 0]
+    [(0 0) 0]
+    [(0 1) 0]
+    [(_ _) (error "invalid signal" s)]))
+
+
+;; ex 3.28 define or-gate
+
+(define (or-gate a1 a2 out)
+  (define (and-action-procedure)
+    (let ([new-val
+           (logical-or (get-signal a1) (get-signal a2))])
+      (after-delay or-gate-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-or a b)
+  (match* (a b)
+    [(1 1) 1]
+    [(1 0) 1]
+    [(0 0) 0]
+    [(0 1) 1]
+    [(_ _) (error "invalid signal" s)]))
