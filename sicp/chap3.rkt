cdb8eb627e0b4ac62c559f328708f5e4e6be5b4d
Shou Ya <shouyatf@gmail.com>
2015-08-23 23:20:54 -0500

    3.35

--- sicp/chap3.rkt
+++ sicp/chap3.rkt
@@ -1119,4 +1119,24 @@ becomes:
 ;; caculated.
 
 
-;;
+;; 3.35 complete the correct squarer
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+        (if (< (get-value b) 0)
+            (error "square less than 0 -- SQUARER" (get-value b))
+            (set-value! a (sqrt (get-value b)) me))
+        (when (has-value? a)
+          (set-value! b (* (get-value a) (get-value a)) me))))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me request)
+    (cond [(eq? request 'i-have-a-value)  (process-new-value)]
+          [(eq? request 'i-lost-my-value) (process-forget-value)]
+          [else (error "unknown request -- squarer" request)]))
+  (connect a me)
+  (connect b me)
+  me)
