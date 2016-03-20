e35aa5873877543f7e85405fcff66ff9d4f6fa94
Shou Ya <shouyatf@gmail.com>
2016-03-20 17:24:11 -0500

    stack new

---
+++ neural-nets/README.md
@@ -0,0 +1,6 @@
+I really interested on how neural network works. Here's an
+introductory guide I follow to implement a basic neural netowrks.
+
+See:
+[Neural Networks and Deep Learning](http://neuralnetworksanddeeplearning.com)
+by [Michael Nielson](http://michaelnielsen.org).
---
+++ neural-nets/src/Neural.hs
@@ -0,0 +1,16 @@
+import System.Random
+import Data.Vector
+
+
+data Network = Network
+  { layers :: [Int]
+  , biases :: [Vector Float]
+  , weights :: [Vector (Vector Float)]
+  }
+
+
+
+createNetwork :: [Int] -> IO Network
+createNetwork lyrs =
+  Network { layers = lyrs
+          , biases = (tail lyrs)}
