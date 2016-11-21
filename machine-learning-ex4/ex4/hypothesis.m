1c24628bbb58345982470bfede9639b6f6483760
Shou Ya <github@lain.li>
2016-11-21 14:08:12 +0800

    remove files that are no longer in use

--- machine-learning-ex4/ex4/hypothesis.m
+++
@@ -1,9 +0,0 @@
-function h = hypothesis(Theta1, Theta2, X)
-  m = size(X, 1);
-  num_labels = size(Theta2, 1);
-
-  p = zeros(size(X, 1), 1);
-
-  h1 = sigmoid([ones(m, 1) X] * Theta1');
-  h = sigmoid([ones(m, 1) h1] * Theta2');
-end
