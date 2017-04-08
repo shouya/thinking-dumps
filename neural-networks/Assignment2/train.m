b1b899902b3c29349d82a2a9ffc000857b2739d9
Shou Ya <github@lain.li>
2017-04-09 02:45:42 +0800

    finishing assignment 2

--- neural-networks/Assignment2/train.m
+++ neural-networks/Assignment2/train.m
@@ -30,7 +30,7 @@ show_validation_CE_after = 1000;
 % LOAD DATA.
 [train_input, train_target, valid_input, valid_target, ...
   test_input, test_target, vocab] = load_data(batchsize);
-[numwords, batchsize, numbatches] = size(train_input); 
+[numwords, batchsize, numbatches] = size(train_input);
 vocab_size = size(vocab, 2);
 
 % INITIALIZE WEIGHTS AND BIASES.
@@ -49,6 +49,9 @@ output_bias_delta = zeros(vocab_size, 1);
 expansion_matrix = eye(vocab_size);
 count = 0;
 tiny = exp(-30);
+
+this_chunk_CE = 0;
+trainset_CE = 0;
 
 % TRAIN.
 for epoch = 1:epochs
@@ -102,14 +105,14 @@ for epoch = 1:epochs
     embed_to_hid_weights_gradient = zeros(numhid1 * numwords, numhid2);
     % Options:
     % (a) embed_to_hid_weights_gradient = back_propagated_deriv_1' * embedding_layer_state;
-    % (b) embed_to_hid_weights_gradient = embedding_layer_state * back_propagated_deriv_1';
+    embed_to_hid_weights_gradient = embedding_layer_state * back_propagated_deriv_1';
     % (c) embed_to_hid_weights_gradient = back_propagated_deriv_1;
     % (d) embed_to_hid_weights_gradient = embedding_layer_state;
 
     % FILL IN CODE. Replace the line below by one of the options.
     hid_bias_gradient = zeros(numhid2, 1);
     % Options
-    % (a) hid_bias_gradient = sum(back_propagated_deriv_1, 2);
+    hid_bias_gradient = sum(back_propagated_deriv_1, 2);
     % (b) hid_bias_gradient = sum(back_propagated_deriv_1, 1);
     % (c) hid_bias_gradient = back_propagated_deriv_1;
     % (d) hid_bias_gradient = back_propagated_deriv_1';
@@ -117,7 +120,7 @@ for epoch = 1:epochs
     % FILL IN CODE. Replace the line below by one of the options.
     back_propagated_deriv_2 = zeros(numhid2, batchsize);
     % Options
-    % (a) back_propagated_deriv_2 = embed_to_hid_weights * back_propagated_deriv_1;
+    back_propagated_deriv_2 = embed_to_hid_weights * back_propagated_deriv_1;
     % (b) back_propagated_deriv_2 = back_propagated_deriv_1 * embed_to_hid_weights;
     % (c) back_propagated_deriv_2 = back_propagated_deriv_1' * embed_to_hid_weights;
     % (d) back_propagated_deriv_2 = back_propagated_deriv_1 * embed_to_hid_weights';
@@ -129,7 +132,7 @@ for epoch = 1:epochs
          expansion_matrix(:, input_batch(w, :)) * ...
          (back_propagated_deriv_2(1 + (w - 1) * numhid1 : w * numhid1, :)');
     end
-    
+
     % UPDATE WEIGHTS AND BIASES.
     word_embedding_weights_delta = ...
       momentum .* word_embedding_weights_delta + ...
