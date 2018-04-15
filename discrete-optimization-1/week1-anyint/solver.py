04047843c05da6ccc2f6dc186bc3a1f4887e550c
Shou Ya <shouya@users.noreply.github.com>
2018-04-15 22:13:07 +0800

    knapsack

--- discrete-optimization-1/week1-anyint/solver.py
+++ discrete-optimization-1/week1-anyint/solver.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def solve_it(input_data):
-    return '9'
+    return '42'
 
 if __name__ == '__main__':
     print('This script submits the integer: %s\n' % solve_it(''))
