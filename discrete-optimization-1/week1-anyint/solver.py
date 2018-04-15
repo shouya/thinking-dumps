f16cde1343cc454f1fc81efc11d8b48cd7af16c1
Shou Ya <shouya@users.noreply.github.com>
2018-04-15 20:57:01 +0800

    discrete optimizatioin week 1

---
+++ discrete-optimization-1/week1-anyint/solver.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def solve_it(input_data):
+    return '9'
+
+if __name__ == '__main__':
+    print('This script submits the integer: %s\n' % solve_it(''))
+
