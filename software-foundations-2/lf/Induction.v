6371aebd429f6a7da5647944a356f285f0214071
Shou Ya <shouya@users.noreply.github.com>
2021-03-25 10:38:39 +0800

    fix a warning

--- software-foundations-2/lf/Induction.v
+++ software-foundations-2/lf/Induction.v
@@ -891,7 +891,7 @@ Definition manual_grade_for_binary_inverse_b : option (nat*string) := None.
         proof -- that will allow the main proof to make progress.) Don't
         define this using [nat_to_bin] and [bin_to_nat]! *)
 
-Fixpoint double_b(b : bin) : bin :=
+Definition double_b(b : bin) : bin :=
   match b with
   | Z => Z
   | n => B0 n
