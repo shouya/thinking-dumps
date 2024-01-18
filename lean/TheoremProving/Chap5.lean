af5b4636049ffc4d777fb5ebc3eb2fa6f435bffb
Shou Ya <shouya@users.noreply.github.com>
2024-01-18 10:38:30 +0900

    proofs using tactics for chapter 4 exercises

--- lean/TheoremProving/Chap5.lean
+++ lean/TheoremProving/Chap5.lean
@@ -1,3 +1,4 @@
+----- Chapter 3 ---------
 import Std.Logic
 
 example (p q r : Prop) (hp : p)
@@ -136,3 +137,97 @@ example : p ∨ ¬p := by
 example : (((p → q) → p) → p) := by
   by_cases hp : p <;> simp [hp]
   -- magical!
+
+
+------ Chapter 4 ----------
+variable (α : Type) (p q : α → Prop)
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
+  apply Iff.intro
+  . intro h; apply And.intro
+    . intro x; exact (h x).left
+    . intro x; exact (h x).right
+  . intro h; intro x
+    exact ⟨h.left x, h.right x⟩
+
+--- functional proof
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
+  apply Iff.intro <;> intros <;> simp [*]
+
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := by
+  intros; simp [*]
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := by
+  intros h; cases h <;> simp [*]
+
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : α → ((∀ _x : α, r) ↔ r) := by
+  intros a; apply Iff.intro
+  . intro f; exact f a
+  . intros r _; assumption
+
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+  by_cases hr : r <;> simp [hr]
+
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
+  apply Iff.intro <;> intros <;> simp [*]
+
+namespace Russell -- avoid polluting the namespace
+variable (men : Type) (barber : men)
+variable (shaves : men → men → Prop)
+
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := by
+  by_cases hx : shaves barber barber
+  . have ⟨h1, _h2⟩ := h barber
+    have hx' := h1 hx
+    contradiction
+  . have ⟨_h1, h2⟩ := h barber
+    have hx' := h2 hx
+    contradiction
+
+open Classical
+
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : (∃ _x : α, r) → r := by intro ⟨_x, r⟩; assumption
+example (a : α) : r → (∃ _x : α, r) := by intro r; exact ⟨a, r⟩
+
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := by
+  apply Iff.intro
+  . intro ⟨x, ⟨px, r⟩⟩; exact ⟨⟨x, px⟩, r⟩
+  . intro ⟨⟨x, px⟩, r⟩; exact ⟨x, ⟨px, r⟩⟩
+
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := by
+  apply Iff.intro
+  . intro ⟨x, pq⟩; cases pq
+    . apply Or.inl; constructor <;> assumption
+    . apply Or.inr; constructor <;> assumption
+  . intro pq; match pq with
+    | Or.inl ⟨x, px⟩ => exact ⟨x, Or.inl px⟩
+    | Or.inr ⟨x, qx⟩ => exact ⟨x, Or.inr qx⟩
+
+theorem not_exist_nonred_apple_iff_all_apples_red
+  : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := by simp only [not_exists, not_not]
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := by simp only [not_exists, not_not]
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := by simp only [not_forall, not_not]
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := by simp only [not_exists]
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := by simp only [not_forall]
+example : (∀ x, (p x → r)) ↔ ((∃ x, p x) → r) := by simp only [forall_exists_index]
+
+example (a : α) : (∃ x, (p x → r)) ↔ ((∀ x, p x) → r) := by
+  apply Iff.intro
+  . intro ⟨x, pxr⟩ hpx; simp [*]
+  . intro h; by_cases hpx : ∀ x, p x
+    . simp [*] at *; constructor <;> first | simp | assumption
+    . have ⟨x, np⟩ := not_forall.mp hpx; clear hpx
+      exact ⟨x, fun px => absurd px np⟩
+
+example (a : α) : (∃ x, (r → p x)) ↔ (r → (∃ x, p x)) := by
+  apply Iff.intro
+  . intros; simp [*] at *; assumption
+  . intro h; by_cases hr : r
+    . simp [*] at *
+    . simp [*] at *; constructor <;> assumption
